b0492a7a710ed82e263bffff39d0d78cf03bfbfe
sharatkatwa <sjkatwa2000@gmail.com>
2023-06-01 11:54:17 +0530

    implimented quick sort and answer some questions about sorting

--- 23_exercise-quick-sort.js
+++ 23_exercise-quick-sort.js
@@ -1,6 +1,40 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
-function quickSort(array, left, right) {}
+function quickSort(array, left, right) {
+  let pivot
+  let partitionIndex
+
+  if (left < right) {
+    pivot = right
+    partitionIndex = partition(array, pivot, left, right)
+
+    quickSort(array, left, partitionIndex - 1)
+    quickSort(array, partitionIndex + 1, right)
+  }
+  return array
+}
+
+function partition(array, pivot, left, right) {
+  pivotValue = array[pivot]
+  partitionIndex = left
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      const temp = array[i]
+      array[i] = array[partitionIndex]
+      array[partitionIndex] = temp
+
+      partitionIndex++
+    }
+  }
+
+  // swap pivot with partition index to set the right position of pivot
+  const temp = array[partitionIndex]
+  array[partitionIndex] = array[right]
+  array[right] = temp
+
+  return partitionIndex
+}
 
 //Select first and last index as 2nd and 3rd parameters
 quickSort(numbers, 0, numbers.length - 1)
---
+++ 24_exercise-sorting-question.js
@@ -0,0 +1,15 @@
+//#1 - Sort 10 schools around your house by distance: //myAns: merge sort
+
+//#2 - eBay sorts listings by the current Bid amount: //myAns: merge sort
+
+//#3 - Sport scores on ESPN: //myAns: merge sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: //myAns: insertion sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: //myAns: insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada: //myAns: merge sort sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.: //myAns: quick sort
+
+//#8 - You want to teach sorting for the first time: //myAns: bubble sort
